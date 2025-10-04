38e4d7b723cec0acf78b68aa2894cc17968f8102
Joshua <joshuaoberlin7@gmail.com>
2025-10-04 10:59:32 +0700

    [ADD] ADD first model Student

---
+++ src/main/java/com/enigma/studentpathfinder/model/Student.java
@@ -0,0 +1,26 @@
+package com.enigma.studentpathfinder.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "student", indexes = {
+        //bikin index @Index(name, column)
+        @Index(name = "idx_student_name", columnList = "name"),
+        @Index(name = "idx_student_email", columnList = "email")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String email;
+
+}
